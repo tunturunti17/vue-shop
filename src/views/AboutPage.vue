c0266f7d01ade731a65878f885e22fac9ad19ac5
Maksim Averyanov <tunturunti17@gmail.com>
2023-08-28 23:35:20 +0300

    страница О Нас

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+        <v-container margin-top="70px">
+            <h1>О магазине</h1>
+            <div>
+                Данный проект магазина создан для закрепления знаний о Vue3
+            </div>
+        </v-container>
+    </v-app>
+</template>
+  <script>
+  </script>
